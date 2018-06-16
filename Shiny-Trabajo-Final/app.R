e9008d0ee64bd29ef7abfa74b049cdb084492dc7
vickysilveira <victoriasilveira16@gmail.com>
2018-06-16 15:20:53 -0300

    Empezando Shiny

---
+++ Shiny-Trabajo-Final/app.R
@@ -0,0 +1,120 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel("Turismo Receptivo"),
+  tabsetPanel(
+    tabPanel("2010",navlistPanel(
+      "Variables Primarias",
+      tabPanel("Destino",
+               h3("Primer panel")
+      ),
+      tabPanel("Alojamiento"),
+      tabPanel("Nacionalidad"),
+      tabPanel("Ingreso")
+    ),navlistPanel(
+      "Variables Secundarias",
+      tabPanel("Cantidad de turistas"),
+      tabPanel("Dias"),
+      tabPanel("Gastos", navlistPanel(
+        "Gastos",
+        tabPanel("Gastos"),
+        tabPanel("Gastos/Persona"),
+        tabPanel("Gastos/Dia c/Persona")))
+    )),
+    tabPanel("2011",navlistPanel(
+      "Variables Primarias",
+      tabPanel("Destino",
+               h3("Primer panel")
+      ),
+      tabPanel("Alojamiento"),
+      tabPanel("Nacionalidad"),
+      tabPanel("Ingreso")
+    ),navlistPanel(
+      "Variables Secundarias",
+      tabPanel("Cantidad de turistas"),
+      tabPanel("Dias"),
+      tabPanel("Gastos", navlistPanel(
+        "Gastos",
+        tabPanel("Gastos"),
+        tabPanel("Gastos/Persona"),
+        tabPanel("Gastos/Dia c/Persona")))
+    )),
+    tabPanel("2012",navlistPanel(
+      "Variables Primarias",
+      tabPanel("Destino",
+               h3("Primer panel")
+      ),
+      tabPanel("Alojamiento"),
+      tabPanel("Nacionalidad"),
+      tabPanel("Ingreso")
+    ),navlistPanel(
+      "Variables Secundarias",
+      tabPanel("Cantidad de turistas"),
+      tabPanel("Dias"),
+      tabPanel("Gastos", navlistPanel(
+        "Gastos",
+        tabPanel("Gastos"),
+        tabPanel("Gastos/Persona"),
+        tabPanel("Gastos/Dia c/Persona")))
+    )),
+    tabPanel("2013",navlistPanel(
+      "Variables Primarias",
+      tabPanel("Destino",
+               h3("Primer panel")
+      ),
+      tabPanel("Alojamiento"),
+      tabPanel("Nacionalidad"),
+      tabPanel("Ingreso")
+    ),navlistPanel(
+      "Variables Secundarias",
+      tabPanel("Cantidad de turistas"),
+      tabPanel("Dias"),
+      tabPanel("Gastos", navlistPanel(
+        "Gastos",
+        tabPanel("Gastos"),
+        tabPanel("Gastos/Persona"),
+        tabPanel("Gastos/Dia c/Persona")))
+    )),
+    tabPanel("2014",navlistPanel(
+      "Variables Primarias",
+      tabPanel("Destino",
+               h3("Primer panel")
+      ),
+      tabPanel("Alojamiento"),
+      tabPanel("Nacionalidad"),
+      tabPanel("Ingreso")
+    ),navlistPanel(
+      "Variables Secundarias",
+      tabPanel("Cantidad de turistas"),
+      tabPanel("Dias"),
+      tabPanel("Gastos", navlistPanel(
+        "Gastos",
+        tabPanel("Gastos"),
+        tabPanel("Gastos/Persona"),
+        tabPanel("Gastos/Dia c/Persona")))
+    )),
+    tabPanel("2015",navlistPanel(
+      "Variables Primarias",
+      tabPanel("Destino",
+               h3("Primer panel")
+      ),
+      tabPanel("Alojamiento"),
+      tabPanel("Nacionalidad"),
+      tabPanel("Ingreso")
+    ),navlistPanel(
+      "Variables Secundarias",
+      tabPanel("Cantidad de turistas"),
+      tabPanel("Dias"),
+      tabPanel("Gastos", navlistPanel(
+        "Gastos",
+        tabPanel("Gastos"),
+        tabPanel("Gastos/Persona"),
+        tabPanel("Gastos/Dia c/Persona")))
+    )),
+    tabPanel("Linea Temporal")
+  )
+)
+server <- function(input, output){}
+shinyApp(ui = ui, server = server)
+
+
