3d60f15ac463b56f3609c0d58ac3d0b05063586b
vickysilveira <victoriasilveira16@gmail.com>
2018-06-24 18:24:09 -0300

    actualizando

--- Shiny-Trabajo-Final/app.R
+++ Shiny-Trabajo-Final/app.R
@@ -240,38 +240,38 @@ ui <- fluidPage(
         tabPanel("Gastos", navlistPanel(
           "Gastos",
           tabPanel("Gastos",plotOutput("plot.2015.d.g.g")),
-          tabPanel("Gastos/Persona",plotOutput("plot.2015.s.g.gp")),
+          tabPanel("Gastos/Persona",plotOutput("plot.2015.d.g.gp")),
           tabPanel("Gastos/Dia c/Persona",plotOutput("plot.2015.d.g.gdp")))))),
       
       tabPanel("Alojamiento",navlistPanel(
         "Variables Secundarias",
-        tabPanel("Cantidad de turistas",plotOutput("2015.a.ct")),
-        tabPanel("Dias",plotOutput("2015.a.d")),
+        tabPanel("Cantidad de turistas",plotOutput("plot.2015.a.ct")),
+        tabPanel("Dias",plotOutput("plot.2015.a.d")),
         tabPanel("Gastos", navlistPanel(
           "Gastos",
-          tabPanel("Gastos",plotOutput("2015.a.g.g")),
-          tabPanel("Gastos/Persona",plotOutput("2015.a.g.gp")),
-          tabPanel("Gastos/Dia c/Persona",plotOutput("2015.a.g.gdp")))))),
+          tabPanel("Gastos",plotOutput("plot.2015.a.g.g")),
+          tabPanel("Gastos/Persona",plotOutput("plot.2015.a.g.gp")),
+          tabPanel("Gastos/Dia c/Persona",plotOutput("plot.2015.a.g.gdp")))))),
       
       tabPanel("Nacionalidad",navlistPanel(
         "Variables Secundarias",
-        tabPanel("Cantidad de turistas",plotOutput("2015.n.ct")),
-        tabPanel("Dias",plotOutput("2015.n.d")),
+        tabPanel("Cantidad de turistas",plotOutput("plot.2015.n.ct")),
+        tabPanel("Dias",plotOutput("plot.2015.n.d")),
         tabPanel("Gastos", navlistPanel(
           "Gastos",
-          tabPanel("Gastos",plotOutput("2015.n.g.g")),
-          tabPanel("Gastos/Persona",plotOutput("2015.n.g.gp")),
-          tabPanel("Gastos/Dia c/Persona",plotOutput("2015.n.g.gdp")))))),
+          tabPanel("Gastos",plotOutput("plot.2015.n.g.g")),
+          tabPanel("Gastos/Persona",plotOutput("plot.2015.n.g.gp")),
+          tabPanel("Gastos/Dia c/Persona",plotOutput("plot.2015.n.g.gdp")))))),
       
       tabPanel("Ingreso",navlistPanel(
         "Variables Secundarias",
-        tabPanel("Cantidad de turistas",plotOutput("2015.i.ct")),
-        tabPanel("Dias",plotOutput("2015.i.d")),
+        tabPanel("Cantidad de turistas",plotOutput("plot.2015.i.ct")),
+        tabPanel("Dias",plotOutput("plot.2015.i.d")),
         tabPanel("Gastos", navlistPanel(
           "Gastos",
-          tabPanel("Gastos",plotOutput("2015.i.g.g")),
-          tabPanel("Gastos/Persona",plotOutput("2015.i.g.gp")),
-          tabPanel("Gastos/Dia c/Persona",plotOutput("2015.i.g.gdp"))))))
+          tabPanel("Gastos",plotOutput("plot.2015.i.g.g")),
+          tabPanel("Gastos/Persona",plotOutput("plot.2015.i.g.gp")),
+          tabPanel("Gastos/Dia c/Persona",plotOutput("plot.2015.i.g.gdp"))))))
     )
     )
   )
@@ -279,32 +279,119 @@ ui <- fluidPage(
 
 
 server <- function(input, output){
-  
+ #2015
+  #Destino
   output$plot.2015.d.ct<- renderPlot({d.por.p %>% ggplot() + 
       geom_col(aes(x=fct_reorder(Destino,value, .desc = TRUE), y= value, fill=Destino)) + 
-      coord_flip()+
-      theme(legend.position="none")+
-      labs(x = "Zona de destino", y = "Poroporci?n de turistas")+ 
-      theme_classic() })
+      coord_flip() +
+      labs(x = "Zona de destino", y = "Poroporci?n de turistas") + 
+      theme_classic() +
+      theme(legend.position="none")})
+  
   output$plot.2015.d.d<- renderPlot({destino2015%>% ggplot() + 
       theme_classic()+
       geom_col(aes(Destino,dias,fill=Destino)) + 
+      theme(legend.position="none") +
       coord_flip()})
+  
   output$plot.2015.d.g.g<- renderPlot({destino2015%>% ggplot() + 
       geom_col(aes(Destino,Gastos,fill=Destino)) + 
       coord_flip()+ 
       theme_classic()+ 
+      theme(legend.position="none") +
       labs(y="Gastos Totales")})
+  
   output$plot.2015.d.g.gp<- renderPlot({destino2015%>% ggplot() + 
       geom_col(aes(Destino,Gastos_P,fill=Destino)) + 
       coord_flip() + 
       theme_classic() + 
+      theme(legend.position="none") +
       labs(y="Gastos Por Persona")})
-  output$plot.2015.d.g.gpd<- renderPlot({destino2015%>% ggplot() + 
+  
+  output$plot.2015.d.g.gdp<- renderPlot({destino2015%>% ggplot() + 
       geom_col(aes(Destino,Gastos_PDia,fill=Destino)) + 
       coord_flip() + 
       theme_classic() + 
+      theme(legend.position="none") +
       labs(y="Gastos Por Persona Por Dia")})
+  
+  #Alojamiento
+  
+  output$plot.2015.a.ct<- renderPlot({a.por.p %>% ggplot() + 
+      geom_col(aes(x=fct_reorder(ALOJA,value, .desc = TRUE),value,fill=ALOJA)) + 
+      labs(x = "Alojamiento utilizado durante su estad?a en el pa?s", y = "Proporci?n de visitantes") + 
+      theme_classic() + 
+      theme(legend.position = "none") +
+      coord_flip()})
+  
+  output$plot.2015.a.d<- renderPlot({aloja2015%>% ggplot() + 
+      geom_col(aes(x=fct_reorder(ALOJA,dias, .desc = TRUE),dias,fill=ALOJA)) + 
+      coord_flip() + 
+      theme_classic() + 
+      theme(legend.position = "none") + 
+      labs(x="Alojamiento utilizado durante su estad?a en el pa?s")})
+  
+  output$plot.2015.a.g.g<- renderPlot({aloja2015%>% ggplot() + 
+      geom_col(aes(x=fct_reorder(ALOJA,Gastos, .desc = TRUE),Gastos,fill=ALOJA)) + 
+      coord_flip() + 
+      theme_classic() + 
+      theme(legend.position = "none") + 
+      labs(x="Alojamiento utilizado durante su estad?a en el pa?s", y="Gastos Totales")})
+  
+  output$plot.2015.a.g.gp<- renderPlot({aloja2015%>% ggplot() + 
+      geom_col(aes(x=fct_reorder(ALOJA,Gastos_P, .desc = TRUE),Gastos_P,fill=ALOJA)) + 
+      coord_flip() + 
+      theme_classic() + 
+      theme(legend.position = "none") + 
+      labs(x="Alojamiento utilizado durante su estad?a en el pa?s", y="Gastos por persona")})
+  
+  output$plot.2015.a.g.gdp<- renderPlot({aloja2015%>% ggplot() + 
+      geom_col(aes(x=fct_reorder(ALOJA,Gastos_PDia, .desc = TRUE),Gastos_PDia,fill=ALOJA)) + 
+      coord_flip() + 
+      theme_classic() + 
+      theme(legend.position = "none") + 
+      labs(x="Alojamiento utilizado durante su estad?a en el pa?s", y="Gastos por persona por dia")})
+  
+  #Nacionalidad
+  
+  output$plot.2015.n.ct<- renderPlot({n.por.p %>% ggplot() + 
+      geom_col(aes(x=fct_reorder(NACIONALIDAD,value, .desc = TRUE), y= value, fill = NACIONALIDAD)) + 
+      coord_flip() + 
+      labs(x = "Nacionalidad", y = "Proporci?n de visitantes") + 
+      theme_classic() +
+      theme(legend.position="none")})
+  
+  output$plot.2015.n.d<- renderPlot({nacionalidad2015%>% ggplot() + 
+      geom_col(aes(x=fct_reorder(NACIONALIDAD,dias, .desc = TRUE),dias,fill=NACIONALIDAD)) + 
+      coord_flip() + 
+      theme_classic() + 
+      labs(x="Nacionalidad") +
+      theme(legend.position = "none")})
+  
+  output$plot.2015.n.g.g<- renderPlot({nacionalidad2015%>% ggplot() + 
+      geom_col(aes(x=fct_reorder(NACIONALIDAD,Gastos, .desc = TRUE),Gastos,fill=NACIONALIDAD)) + 
+      coord_flip() + 
+      theme_classic() + 
+      theme(legend.position = "none") + 
+      labs(x="Nacionalidad") +
+      labs(y="Gastos Totales")})
+  
+  output$plot.2015.n.g.gp<- renderPlot({nacionalidad2015%>% ggplot() + 
+      geom_col(aes(x=fct_reorder(NACIONALIDAD,Gastos_P, .desc = TRUE),Gastos_P,fill=NACIONALIDAD)) + 
+      coord_flip() + 
+      theme_classic() + 
+      theme(legend.position = "none") + 
+      labs(x="Nacionalidad") +
+      labs(y="Gastos por persona")})
+  
+  output$plot.2015.n.g.gdp<- renderPlot({nacionalidad2015%>% ggplot() + 
+      geom_col(aes(x=fct_reorder(NACIONALIDAD,Gastos_PDia, .desc = TRUE),Gastos_PDia,fill=NACIONALIDAD)) + 
+      coord_flip() + 
+      theme_classic() + 
+      theme(legend.position = "none") + 
+      labs(x="Nacionalidad") +
+      labs(y="Gastos por persona por dia")})
+
 
 }
 
